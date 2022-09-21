230fe5abfe28ddf75954e0a67226374589f938ee
sjw-steven <sjw141013@gmail.com>
2022-09-21 22:00:13 +0800

    add hook listen file

---
+++ listen.php
@@ -0,0 +1,29 @@
+<?php
+
+$rawdata = file_get_contents('php://input');
+
+$payload = json_decode($rawdata);
+
+switch (strtolower($_SERVER['HTTP_X_GITHUB_EVENT'])) {
+    case 'ping':
+        echo 'pong';
+        exec('cd /var/www/html/wp-content/themes/u3a;git pull');
+        echo "git pull done";
+        break;
+
+    case 'pull_request':
+        switch ($payload->{'action'}) {
+            case 'opened':
+            case 'closed':
+                exec('cd /var/www/html/wp-content/themes/u3a;git pull');
+                echo "git pull done";
+                break;
+
+        }
+        break;
+
+    default:
+        header('HTTP/1.0 404 Not Found');
+        die();
+}
+?>
