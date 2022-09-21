b7706079de83adc5efe6012d33be72434d9557ad
sjw-steven <sjw141013@gmail.com>
2022-09-21 22:52:19 +0800

    update listen

--- listen.php
+++ listen.php
@@ -6,11 +6,9 @@
 $rawdata = file_get_contents('php://input');
 
 if ($rawdata) {
-    file_put_contents("/var/www/html/wp-content/themes/u3a/listen.log", $rawdata);
-
     $payload = json_decode($rawdata);
 
-    switch (strtolower($_SERVER['HTTP_X_GITHUB_EVENT'])) {
+    switch (strtolower($_SERVER['X-GitHub-Event'])) {
         case 'ping':
             echo 'pong';
             exec('cd /var/www/html/wp-content/themes/u3a;git pull');
