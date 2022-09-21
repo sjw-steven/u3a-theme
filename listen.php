e2aa5ec3200926c7aeb5f8c7f62d6ecba58016ec
sjw-steven <sjw141013@gmail.com>
2022-09-22 00:24:20 +0800

    edit listen function

--- listen.php
+++ listen.php
@@ -9,21 +9,9 @@ if ($rawdata) {
     $payload = json_decode($rawdata);
 
     switch (strtolower($_SERVER['HTTP_X_GITHUB_EVENT'])) {
-        case 'ping':
-            echo 'pong';
+        case 'push':
             exec('cd /var/www/html/wp-content/themes/u3a;git pull');
             echo "git pull done";
-            break;
-
-        case 'push':
-            switch ($payload->{'action'}) {
-                case 'opened':
-                case 'closed':
-                    exec('cd /var/www/html/wp-content/themes/u3a;git pull');
-                    echo "git pull done";
-                    break;
-
-            }
             break;
 
         default:
