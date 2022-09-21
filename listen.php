05b9bc2b03e52974f8d1a626dc2eee9df6bb6a79
sjw-steven <sjw141013@gmail.com>
2022-09-21 22:28:51 +0800

    update listen file

--- listen.php
+++ listen.php
@@ -2,28 +2,32 @@
 
 $rawdata = file_get_contents('php://input');
 
-$payload = json_decode($rawdata);
+if ($rawdata) {
+    $payload = json_decode($rawdata);
 
-switch (strtolower($_SERVER['HTTP_X_GITHUB_EVENT'])) {
-    case 'ping':
-        echo 'pong';
-        exec('cd /var/www/html/wp-content/themes/u3a;git pull');
-        echo "git pull done";
-        break;
+    switch (strtolower($_SERVER['HTTP_X_GITHUB_EVENT'])) {
+        case 'ping':
+            echo 'pong';
+            exec('cd /var/www/html/wp-content/themes/u3a;git pull');
+            echo "git pull done";
+            break;
 
-    case 'pull_request':
-        switch ($payload->{'action'}) {
-            case 'opened':
-            case 'closed':
-                exec('cd /var/www/html/wp-content/themes/u3a;git pull');
-                echo "git pull done";
-                break;
+        case 'pull_request':
+            switch ($payload->{'action'}) {
+                case 'opened':
+                case 'closed':
+                    exec('cd /var/www/html/wp-content/themes/u3a;git pull');
+                    echo "git pull done";
+                    break;
 
-        }
-        break;
+            }
+            break;
 
-    default:
-        header('HTTP/1.0 404 Not Found');
-        die();
+        default:
+            header('HTTP/1.0 404 Not Found');
+            die();
+    }
+} else {
+    echo "Error Request";
 }
 ?>
